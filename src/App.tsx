ce399ea3dd783592ebb348b45e25526b17bd7669
JrzenonDev <jribertoonb@gmail.com>
2023-07-07 03:08:04 -0300

    fix: eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import './App.css';
+import './App.css'
 
 function App() {
+
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </>
-  );
+    <div className='min-h-screen text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400'>
+      <h1>Hello</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
