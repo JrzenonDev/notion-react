51b06a6e9ddfba356da26e7fdf8562f22f847139
JrzenonDev <jribertoonb@gmail.com>
2023-07-07 02:09:16 -0300

    fix: adjusts in lint and prettier corretion conflicts in verion packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <h1>Hello</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
